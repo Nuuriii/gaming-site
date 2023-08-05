227e2e575b259affb4c021bc5a67a20ea74d6447
Nurrohmann <nurrohmanmarduki@gmail.com>
2023-08-05 12:05:25 +0700

    feat: add property image in object

---
+++ src/components/learning/learningData.ts
@@ -0,0 +1,40 @@
+import MobileGameDev from "../../../public/smartphone.svg";
+import PcGameDev from "../../../public/pc.svg";
+import Ps4 from "../../../public/ps4.svg";
+import Vr from "../../../public/vr.svg";
+import VrDesign from "../../../public/dekstopcube.svg";
+import Model from "../../../public/cube.svg";
+import Arrow from "../../../public/arrow.svg";
+
+export const LearningPath = [
+   {
+      img: MobileGameDev,
+      title: "Mobile Game Development",
+      icon: Arrow,
+   },
+   {
+      img: PcGameDev,
+      title: "PC Game Development",
+      icon: Arrow,
+   },
+   {
+      img: Ps4,
+      title: "PS4 Game Development",
+      icon: Arrow,
+   },
+   {
+      img: Vr,
+      title: "AR/VR Solutions",
+      icon: Arrow,
+   },
+   {
+      img: VrDesign,
+      title: "AR/ VR design",
+      icon: Arrow,
+   },
+   {
+      img: Model,
+      title: "3D Modelings",
+      icon: Arrow,
+   },
+];
