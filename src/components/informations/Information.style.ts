d9c0b622e74aa159480b58e03875c34ac9046af2
Nurrohmann <nurrohmanmarduki@gmail.com>
2023-08-05 07:54:16 +0700

    feat: add style in FirstInformation Component

--- src/components/informations/Information.style.ts
+++ src/components/informations/Information.style.ts
@@ -1,12 +1,20 @@
 import styled from "styled-components";
 
 export const WrapperInfo = styled.div`
+   @import url("https://fonts.googleapis.com/css2?family=Poppins:wght@400;600;700&display=swap");
    display: flex;
    flex-direction: column;
+   align-items: center;
+   padding: 0 5rem;
+   background: linear-gradient(to bottom, rgb(106, 78, 209), rgb(71, 29, 187));
 `;
 
 export const FirstInfo = styled.div`
    display: flex;
+   font-family: Poppins, sans;
+   color: white;
+   text-align: center;
+   font-weight: 600;
 `;
 
 export const SecondInfo = styled.div`
