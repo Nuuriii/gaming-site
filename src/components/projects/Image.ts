2588a0afee265a4e13d7267482dcd7ad13120cdd
Nurrohmann <nurrohmanmarduki@gmail.com>
2023-08-06 11:20:04 +0700

    feat: create array for save image

---
+++ src/components/projects/Image.ts
@@ -0,0 +1,27 @@
+import Image1 from "../../../public/image1.svg";
+import Image2 from "../../../public/image2.svg";
+import Image3 from "../../../public/image3.svg";
+import Image4 from "../../../public/image4.svg";
+import Image5 from "../../../public/image5.svg";
+import Image6 from "../../../public/image6.svg";
+
+export const Image = [
+   {
+      image: Image1,
+   },
+   {
+      image: Image2,
+   },
+   {
+      image: Image3,
+   },
+   {
+      image: Image4,
+   },
+   {
+      image: Image5,
+   },
+   {
+      image: Image6,
+   },
+];
