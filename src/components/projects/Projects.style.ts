42413f43fae3ab8ef14ead97f5a01418b78840c8
Nurrohmann <nurrohmanmarduki@gmail.com>
2023-08-07 08:56:08 +0700

    feat: centering element

--- src/components/projects/Projects.style.ts
+++ src/components/projects/Projects.style.ts
@@ -8,10 +8,29 @@ export const ImageProjects = styled.div`
 //for LoopImage Component
 export const ProjectWrapper = styled.div`
    display: flex;
-   margin: 0.8rem;
+   margin: 0.7rem;
 `;
 
 export const ProjectContainer = styled.div`
    display: flex;
    flex-wrap: wrap;
+   justify-content: center;
+`;
+
+//style for ShowProjects Component
+export const MainProjects = styled.div`
+   display: flex;
+   flex-direction: column;
+   align-items: center;
+   background: linear-gradient(to bottom, rgb(106, 78, 209), rgb(71, 29, 187));
+   width: 100vw;
+   justify-content: center;
+`;
+
+export const DescriptionProject = styled.div`
+   display: flex;
+   flex-direction: column;
+   align-items: center;
+   font-family: sans;
+   color: white;
 `;
--- src/components/projects/ShowProjects.tsx
+++ src/components/projects/ShowProjects.tsx
@@ -1,10 +1,21 @@
 import { Image } from "./Image";
 import { LoopImage } from "./LoopImage";
+import { Container } from "../heroSections/Hero.style";
+import { MainProjects, DescriptionProject } from "./Projects.style";
 
 export const ShowProjects = () => {
    return (
-      <div>
-         <LoopImage projectData={Image} />
-      </div>
+      <Container>
+         <MainProjects>
+            <DescriptionProject>
+               <h1>Our Recent Projects</h1>
+               <p>
+                  Lorem Ipsum is simply dummy text of the printing and
+                  typesetting industry.{" "}
+               </p>
+            </DescriptionProject>
+            <LoopImage projectData={Image} />
+         </MainProjects>
+      </Container>
    );
 };
