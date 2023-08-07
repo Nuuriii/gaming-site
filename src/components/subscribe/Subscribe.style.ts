5444edceab94aa761fa776d5fae678f666c1c6a1
Nurrohmann <nurrohmanmarduki@gmail.com>
2023-08-08 00:50:19 +0700

    feat: add Subscribe component

---
+++ src/components/subscribe/Subscribe.style.ts
@@ -0,0 +1,18 @@
+import styled from "styled-components";
+
+export const SubscribeWrapper = styled.div`
+   display: flex;
+   flex-direction: column;
+`;
+
+export const SubscribeInformation = styled.div`
+   display: flex;
+   flex-direction: column;
+`;
+
+export const SubscribeForm = styled.div`
+   display: flex;
+   background-color: #1c140f;
+   border-radius: 0%.3rem;
+   padding: 1rem;
+`;
---
+++ src/components/subscribe/Subscribe.tsx
@@ -0,0 +1,20 @@
+import { Container } from "../heroSections/Hero.style";
+import {
+   SubscribeWrapper,
+   SubscribeInformation,
+   SubscribeForm,
+} from "./Subscribe.style";
+
+export const Subscribe = () => {
+   return (
+      <Container>
+         <div>
+            <div></div>
+            <div>
+               <div></div>
+               <div></div>
+            </div>
+         </div>
+      </Container>
+   );
+};
