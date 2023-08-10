b33dcfe50cd8c593393adbb0926eb234b751d8cc
Nurrohmann <nurrohmanmarduki@gmail.com>
2023-08-11 06:54:46 +0700

    feat: add styled components for Navbar Components

---
+++ src/components/nav/Navbar.style.ts
@@ -0,0 +1,13 @@
+import styled from "styled-components";
+
+export const NavWrapper = styled.div`
+   display: flex;
+`;
+
+export const BrandLogo = styled.div`
+   display: flex;
+`;
+
+export const Navigation = styled.ul`
+   display: flex;
+`;
--- src/components/nav/Navbar.tsx
+++ src/components/nav/Navbar.tsx
@@ -1,16 +1,19 @@
+import { Container } from "../heroSections/Hero.style";
+import { NavWrapper, BrandLogo, Navigation } from "./Navbar.style";
+
 export const Navbar = () => {
    return (
-      <>
-         <div>
-            <div>logo</div>
-            <div>
+      <Container>
+         <NavWrapper>
+            <BrandLogo>logo</BrandLogo>
+            <Navigation>
                <a href=''>Home</a>
                <a href=''>About Us</a>
                <a href=''>Portfolio</a>
                <a href=''>News</a>
                <button>Contact Us</button>
-            </div>
-         </div>
-      </>
+            </Navigation>
+         </NavWrapper>
+      </Container>
    );
 };
