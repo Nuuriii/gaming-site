fe6add16d9b35176d213827427d7cc250572ac40
Nurrohmann <nurrohmanmarduki@gmail.com>
2023-08-11 06:25:56 +0700

    feat: add Navbar Component

---
+++ src/components/nav/Navbar.tsx
@@ -0,0 +1,16 @@
+export const Navbar = () => {
+   return (
+      <>
+         <div>
+            <div>logo</div>
+            <div>
+               <a href=''>Home</a>
+               <a href=''>About Us</a>
+               <a href=''>Portfolio</a>
+               <a href=''>News</a>
+               <button>Contact Us</button>
+            </div>
+         </div>
+      </>
+   );
+};
